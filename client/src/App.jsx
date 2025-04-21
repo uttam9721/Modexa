e0bf469d98c6325523cd27574dde6798c7d7b1c7
Uttam Kumar <uttammaurya377@gmail.com>
2025-04-22 01:02:17 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import {Routes,Route, BrowserRouter} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Order from './pages/Order'
+import Login from './pages/Login'
+import Navbar from './components/Navbar';
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+     <BrowserRouter>
+      <Navbar path='/navbar' element={<Navbar/>} />
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/collection' element={<Collection/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/product/:productId' element={<Product/>} />
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='/place-order' element={<PlaceOrder/>} />
+      <Route path='/order' element={<Order/>} />
+      <Route path='/login' element={<Login/>} />
+     </Routes>
+     </BrowserRouter> 
+    </div>
   )
 }
 
---
+++ client/src/pages/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+      <h1>PlaceOrder</h1>
+    </div>
+  )
+}
+
+export default PlaceOrder
