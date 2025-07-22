66ac586a9863bb7b759fa14ea26d03d7f02d30d2
uttam9721 <uttammaurya377@gmail.com>
2025-07-23 05:16:00 +0530

    modexa

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './../../admin/src/components/Navbar';
 import Sidebar from './../../admin/src/components/Sidebar';
+import List from './pages/List';
+import Orders from './pages/Orders';
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
           <Routes>
             <Route path="/add" element={<h1 className='p-4'>Add Page</h1>} />
             {/* Add more routes here */}
+            <Route path='/list' element={<List/>} />
+            <Route path='/orders' element={<Orders/>} />
           </Routes>
         </div>
       </Router>
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,19 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+// // import { Link } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
+// // import { assets } from '../../../admin/src/assets/assets'
 // import { assets } from '../../../admin/src/assets/assets'
-import { assets } from '../../../admin/src/assets/assets'
+// const Sidebar = () => {
+//   return (
+//     <div className='w-[18%] min-h-screen border-r-2'>
+//       <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+//         <LinkLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1' to="/add">
+//         <img className='w-5 h-5' src={assets.add_icon} alt="" />
+//         <p className='hidden md:block'>Add Items</p>
+
+//         </LinkLink>
+//         <LinkLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1' to="/list">
+//         <img className='w-5 h-5' src={assets.order_icon} alt="" />
+//         <p className='hidden md:block'>List Items</p>
+
+//         </LinkLink>
+//         <LinkLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1' to="/orders">
+//         <img className='w-5 h-5' src={assets.order_icon} alt="" />
+//         <p className='hidden md:block'>Oders</p>
+
+//         </LinkLink>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Sidebar
+
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { assets } from '../../../admin/src/assets/assets';
+
 const Sidebar = () => {
   return (
     <div className='w-[18%] min-h-screen border-r-2'>
       <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
-        <Link className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1' to="/add">
-        <img className='w-5 h-5' src={assets.add_icon} alt="" />
-        <p className='hidden md:block'>Add Items</p>
 
-        </Link>
+        <NavLink
+          to="/add"
+          className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-md"
+        >
+          <img className='w-5 h-5' src={assets.add_icon} alt="Add" />
+          <p className='hidden md:block'>Add Items</p>
+        </NavLink>
+
+        <NavLink
+          to="/list"
+          className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-md"
+        >
+          <img className='w-5 h-5' src={assets.order_icon} alt="List" />
+          <p className='hidden md:block'>List Items</p>
+        </NavLink>
+
+        <NavLink
+          to="/orders"
+          className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-md"
+        >
+          <img className='w-5 h-5' src={assets.order_icon} alt="Orders" />
+          <p className='hidden md:block'>Orders</p>
+        </NavLink>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
